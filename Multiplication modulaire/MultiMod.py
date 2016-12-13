9551e35186a3439e2965b9fe01506e8a2fad0f4f
Colin <theTI2O@gmail>
2016-12-13 21:00:52 +0100

    La base, après c'est que de l'esbrouffe !

--- Multiplication modulaire/MultiMod.py
+++ Multiplication modulaire/MultiMod.py
@@ -5,3 +5,39 @@
 # Par Erwan Dessailly et Colin Baumgard        #
 #                                              #
 ################################################
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def calculModulo(a, p):
+    l = []
+    for b in range(0,p):
+        l.append([b, (a*b)%p])
+    print(l)
+    return l
+
+
+a = 2
+mod = 100
+
+listeCouples = calculModulo(a,mod)
+
+ax = plt.subplot(111, projection='polar')
+
+nPoints = mod
+nCouples = len(listeCouples)
+
+delta = 2*np.pi/nPoints
+
+for i in range(0, nCouples):
+        alpha = listeCouples[i][0]*delta
+        beta = listeCouples[i][1]*delta
+        print(alpha, beta)
+
+        ax.plot([alpha, beta], [1,1])
+
+
+
+plt.show()
