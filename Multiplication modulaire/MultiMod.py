eeee8f6e876f18b3c26a34288a4ea8e0226377fe
Colin <theTI2O@gmail>
2016-12-16 16:29:46 +0100

    La base poto ! coucou

--- Multiplication modulaire/MultiMod.py
+++ Multiplication modulaire/MultiMod.py
@@ -24,6 +24,8 @@ class Interface(Frame):
 
         # self.pack(fill=BOTH)
 
+        couco = 1
+
         self.fenetre = fenetre
 
         # variable globales
@@ -35,36 +37,85 @@ class Interface(Frame):
         # Zone de graphique:
         self.zoneGraphique = Frame(self.fenetre)
 
-
         self.figure = Figure(figsize=(7,7), dpi=100)
         self.graphique = self.figure.add_subplot(111, projection='polar')
-        self.graphique.plot(1,1)
-
 
         self.canvas = FigureCanvasTkAgg(self.figure, self.zoneGraphique)
         self.canvas.show()
         self.canvas.get_tk_widget().pack()
 
-        self.zoneGraphique.grid(row=0, column=0)
 
 
         # Interface de contrôle
-
         self.zoneDeControle = Frame(self.fenetre)
 
-        # Scrollbar a
-        labelA = Label(self.zoneDeControle, text='a')
-        labelA.grid(row=0, column=0)
-        scaleA = Scale(self.zoneDeControle, variable=self.a, orient='horizontal', command=self.actualiser)
-        scaleA.grid(row=0, column=1)
 
-        # Scrollbar mod
-        labelMod = Label(self.zoneDeControle, text='modulo')
-        labelMod.grid(row=1, column=0)
-        scaleMod = Scale(self.zoneDeControle, variable=self.mod, orient='horizontal', command=self.actualiser)
-        scaleMod.grid(row=1, column=1)
+            #zoneControleStatique
+        self.zoneDeControleStatique = Frame(self.zoneDeControle)
+
+                # Scrollbar a
+        self.labelA = Label(self.zoneDeControleStatique, text='a')
+        self.scaleA = Scale(self.zoneDeControleStatique, variable=self.a, orient='horizontal', command=self.actualiser)
+
+                # Scrollbar mod
+        self.labelMod = Label(self.zoneDeControleStatique, text='modulo')
+        self.scaleMod = Scale(self.zoneDeControleStatique, variable=self.mod, orient='horizontal', command=self.actualiser)
+
+                #construction grille
+        self.labelA.grid(row=0, column=0)
+        self.scaleA.grid(row=0, column=1)
+
+        self.labelMod.grid(row=1, column=0)
+        self.scaleMod.grid(row=1, column=1)
+
+
+            #Zone de controle animation
+        self.zoneDeControleAnimation = Frame(self.zoneDeControle, bd=2)
+
+                #Paramètres:
+                    #on fait varier mod ou a:
+        self.choix = StringVar()
+        self.boutonA = Radiobutton(self.zoneDeControleAnimation, text='a', variable=self.choix, value="a")
+        self.boutonMod = Radiobutton(self.zoneDeControleAnimation, text='modulo', variable=self.choix, value="mod")
+
+
+                    # de... à... par pas de ...
+        self.labelDe = Label(self.zoneDeControleAnimation, text='De: ')
+        self.spinDe = Spinbox(self.zoneDeControleAnimation, from_=0, to=1000)
+        self.labelA = Label(self.zoneDeControleAnimation, text='à: ')
+        self.spinA = Spinbox(self.zoneDeControleAnimation, from_=0, to=1000)
+        self.labelPas = Label(self.zoneDeControleAnimation, text='par pas de: ')
+        self.spinPas = Spinbox(self.zoneDeControleAnimation, from_=0, to=1)
+
+
+                # bouton de lancement
+        self.lancerAnimation = Button(self.zoneDeControleAnimation, text="Lancer", command=self.animation)
+
+                #construction grille:
+        self.boutonA.grid(row=0, column=0)
+        self.boutonMod.grid(row=0, column=1)
+
+        self.labelDe.grid(row=1, column=0)
+        self.spinDe.grid(row=1, column=1)
+        self.labelA.grid(row=1, column=2)
+        self.spinA.grid(row=1, column=3)
+        self.labelPas.grid(row=2, column=0)
+        self.spinPas.grid(row=2, column=1)
+
+        self.lancerAnimation.grid(row=3, column=0)
+
+
+        #construction grille fenetre
+
+        self.zoneGraphique.grid(row=0, column=0)
+        self.zoneDeControleStatique.grid(row=0, column=0, padx=50)
+        self.zoneDeControleAnimation.grid(row=0, column=1, padx=50)
 
         self.zoneDeControle.grid(row=1, column=0)
+
+
+    def animation(self):
+        q = 1
 
 
     def actualiser(self, adsf):
@@ -75,6 +126,7 @@ class Interface(Frame):
 
         self.afficher(a, mod)
 
+
     def calculModulo(self, a, p):
         l = []
         for b in range(0, p):
@@ -83,9 +135,10 @@ class Interface(Frame):
 
     def afficher(self, a, mod):
 
-        listeCouples = self.calculModulo(a, mod)
-
         self.graphique.clear()
+
+
+        listeCouples = self.calculModulo(a, mod)
 
 
         nPoints = mod
