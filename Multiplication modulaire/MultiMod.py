af5123f7f78b5352f88860f5f09f50c8ee0fa817
Colin <theTI2O@gmail>
2016-12-18 14:12:43 +0100

    Je crois que j'ai fait à peu près tout ce que je voulais faire. Il ne reste qu'à mieux dessiner la fenetre (marges, espaces, labels etc...) J'ai du passer par un Thread pour le calcul de l'animation qui n'a malheureusement rien d'une fonction optimisée... Le programme n'est pas très stable pendant le calcul de l'animation mais j'ai un peu la flemme d'y passer encore quelques heures...

--- Multiplication modulaire/MultiMod.py
+++ Multiplication modulaire/MultiMod.py
@@ -17,6 +17,66 @@ import numpy as np
 from tkinter import *
 from tkinter.messagebox import *
 import os
+from threading import Thread
+import time
+
+
+class AnimGenerator(Thread):
+
+    def __init__(self, interface):
+        Thread.__init__(self)
+        self.interface = interface
+        self.quit = False
+
+    def run(self):
+        while not self.quit:
+            time.sleep(0.1)
+
+    def draw(self):
+
+        self.interface.lancerAnimation.config(state=DISABLED)
+
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=self.interface.imageParSeconde, metadata=dict(artist='Colin Baumgard'), bitrate=10000)
+
+        with writer.saving(self.interface.figure, "Animation.mp4", 500):
+
+            for i in range(0, self.interface.nbDeFrames):
+                self.loopAnim()
+                self.interface.canvas.show()
+                writer.grab_frame()
+                self.interface.textAvancement.set("Calcul: " + str(i + 1) + "/" + str(self.interface.nbDeFrames))
+
+
+        os.system("explorer.exe /e," + os.getcwd())
+        self.interface.lancerAnimation.config(state=NORMAL)
+
+    def loopAnim(self):
+        self.interface.graphique.clear()
+        self.interface.figure.suptitle(('Table de {} \nmodulo {}'.format(round(self.interface.aLoop, 2), self.interface.modLoop)))
+        self.interface.graphique.grid(False)
+        self.interface.graphique.axis([0, 1, 0, 1], )
+        self.interface.graphique.set_xticklabels([])
+        self.interface.graphique.set_yticklabels([])
+
+        if self.interface.modLoop != 0:
+            delta = 2 * np.pi / self.interface.modLoop
+        else:
+            delta = 0
+
+        for b in range(0, self.interface.modLoop):
+            alpha = b * delta
+            beta = ((self.interface.aLoop * b) % self.interface.modLoop) * delta
+
+            self.interface.graphique.plot([alpha, beta], [1,1], c='b')
+
+        self.interface.aLoop = round(self.interface.aLoop, 2) + self.interface.pas
+
+        return self.interface.graphique,
+
+    def stop(self):
+        self.quit = True
+
 
 
 class Interface(Frame):
@@ -24,6 +84,10 @@ class Interface(Frame):
     def __init__(self, fenetre, **kwargs):
 
         Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
+
+        self.animGenerator = AnimGenerator(self)
+        self.animGenerator.start()
+
 
         self.imageParSeconde = 12
 
@@ -40,6 +104,11 @@ class Interface(Frame):
 
         self.figure = Figure(figsize=(7, 7), dpi=100)
         self.graphique = self.figure.add_subplot(111, projection='polar')
+        self.graphique.grid(False)
+        self.graphique.axis([0, 1, 0, 1], )
+        self.graphique.set_xticklabels([])
+        self.graphique.set_yticklabels([])
+
 
         self.canvas = FigureCanvasTkAgg(self.figure, self.zoneGraphique)
         self.canvas.show()
@@ -88,12 +157,12 @@ class Interface(Frame):
         self.labelDe = Label(self.zoneDeControleAnimation, text='A variant de: ')
         self.spinDe = Spinbox(self.zoneDeControleAnimation, from_=1, to=1000, increment=10, command=self.verifier)
         self.spinDe.delete(0, 'end')
-        self.spinDe.insert('end', 50)
+        self.spinDe.insert('end', 2)
 
         self.labelA = Label(self.zoneDeControleAnimation, text='à: ')
         self.spinA = Spinbox(self.zoneDeControleAnimation, from_=1, to=1000, increment=10, command=self.verifier)
         self.spinA.delete(0, 'end')
-        self.spinA.insert('end', 200)
+        self.spinA.insert('end', 5)
 
         self.labelDuree = Label(self.zoneDeControleAnimation, text="durée(s): ")
         self.spinDuree = Spinbox(self.zoneDeControleAnimation, from_=1, to=50, increment=1)
@@ -103,7 +172,7 @@ class Interface(Frame):
         self.labelMod = Label(self.zoneDeControleAnimation, text="modulo: ")
         self.spinMod = Spinbox(self.zoneDeControleAnimation, from_=1, to=1000, increment=1)
         self.spinMod.delete(0, 'end')
-        self.spinMod.insert('end', 100)
+        self.spinMod.insert('end', 200)
 
 
                 # bouton de lancement
@@ -138,6 +207,8 @@ class Interface(Frame):
 
         self.zoneDeControle.grid(row=1, column=0)
 
+        self.actualiser()
+
 
 
     def actualiser(self):
@@ -151,6 +222,7 @@ class Interface(Frame):
     def afficher(self, a, mod):
 
         self.graphique.clear()
+        self.figure.suptitle(('Table de {} \nmodulo {}'.format(round(a, 2), mod)))
 
 
         if mod != 0:
@@ -165,10 +237,13 @@ class Interface(Frame):
             beta = ((a * b) % mod) * delta
 
             self.graphique.plot([alpha, beta], [1, 1], c='r')
+            self.graphique.grid(False)
+            self.graphique.axis([0,1,0,1], )
+            self.graphique.set_xticklabels([])
+            self.graphique.set_yticklabels([])
 
         self.canvas.show()
 
-        #self.canvas.get_tk_widget().pack()
 
 
     ###### Fonctions tests Colin ######
@@ -186,10 +261,6 @@ class Interface(Frame):
 
         self.verifier()
 
-        showinfo('Animation', 'Le rendu peut prendre un peu de temps, soyez patient...')
-
-
-
         self.aLoop = float(self.spinDe.get())
         self.modLoop = int(self.spinMod.get())
         limite = float(self.spinA.get())
@@ -199,59 +270,20 @@ class Interface(Frame):
 
         delta = limite - self.aLoop
 
-        self.pas = round(delta/self.nbDeFrames, 2)
+        self.pas = round(delta / self.nbDeFrames, 3)
 
-        #self.figureAnim = Figure(figsize=(10, 10), dpi=500)
-        #self.graphiqueAnim = self.figureAnim.add_subplot(111, projection='polar')
-        #self.canvasAnim = FigureCanvasTkAgg(self.figureAnim, self.zoneGraphique)
+        if askyesno('Animation', 'Récapitulatif:\n'
+                                 '- A variant de {} à {} modulo {}\n'
+                                 '- Durée: {}s\n'
+                                 '- Nombre de calcul: {}\n'
+                                 '- Temps max estimé: {}min\n'
+                                 'Voulez vous lancer la simulation ?'.format(self.aLoop, limite, self.modLoop, duree, self.nbDeFrames, round((self.nbDeFrames*1.5)/60, 2))):
 
-        Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=self.imageParSeconde, metadata=dict(artist='Colin Baumgard'), bitrate=10000)
-
-        if False :
-
-            self.anim = animation.FuncAnimation(self.figure, self.genererAnimation, frames=self.nbDeFrames, interval=int(1000/self.imageParSeconde), blit=False, repeat=False)
-            #self.anim = animation.FuncAnimation(self.figure, self.genererAnimation, frames=10, interval=10, repeat=False)
-
-            self.canvas.show()
-            self.canvas.get_tk_widget().pack()
-            self.anim.save('anim.mp4', writer=writer)
+            self.animGenerator.draw()
 
 
-        else:
-            with writer.saving(self.figure, "Animation.mp4", 500):
-                for i in range(0, self.nbDeFrames):
-                    self.genererAnimation(i)
-                    self.canvas.show()
-                    #self.canvas.get_tk_widget().pack()
-                    writer.grab_frame()
-                    self.textAvancement.set("Calcul: " + str(i+1) +  "/" + str(self.nbDeFrames))
-
-
-
-        os.system("explorer.exe /e,"+ os.getcwd())
-
-
-
-    def genererAnimation(self, i):
-
-        self.graphique.clear()
-        self.figure.suptitle(('Table de {} modulo {}'.format(self.aLoop, self.modLoop)))
-
-        if self.modLoop != 0:
-            delta = 2 * np.pi / self.modLoop
-        else:
-            delta = 0
-
-        for b in range(0, self.modLoop):
-            alpha = b * delta
-            beta = ((self.aLoop * b) % self.modLoop) * delta
-
-            self.graphique.plot([alpha, beta], [1,1], c='b')
-
-        self.aLoop = round(self.aLoop, 2) + self.pas
-
-        return self.graphique,
+    def stop(self):
+        self.animGenerator.stop()
 
 
 
@@ -261,3 +293,5 @@ fenetre.title('MultiMod')
 interface = Interface(fenetre)
 
 interface.mainloop()
+
+interface.stop()
